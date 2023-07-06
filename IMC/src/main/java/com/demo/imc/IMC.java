ddda7b11c8e8958a7faab2313ef532720946e684
Ruben <rubenm1386@gmail.com>
2023-07-06 17:50:44 +0200

    Subiendo el proyecto de IMC con java

---
+++ IMC/src/main/java/com/demo/imc/IMC.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.demo.imc;
+
+/**
+ *
+ * @author ruben
+ */
+public class IMC {
+
+    public static void main(String[] args) {
+        MainWindow main_window = new MainWindow();
+        
+        main_window.setVisible(true);
+    }
+}
